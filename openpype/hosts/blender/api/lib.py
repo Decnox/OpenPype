9618b6aa888494fa9080f168eb8470782c8333bf
Felix David <felixg.david@gmail.com>
2022-08-19 19:27:42 +0200

    Build assets library

--- openpype/hosts/blender/api/lib.py
+++ openpype/hosts/blender/api/lib.py
@@ -28,8 +28,14 @@ from openpype.pipeline import (
     get_current_asset_name,
     get_current_task_name,
 )
-from openpype.lib import Logger
-from openpype.pipeline import legacy_io, schema
+from openpype.pipeline import legacy_io, Anatomy, schema
+from openpype.pipeline.constants import AVALON_CONTAINER_ID
+from openpype.client.entities import (
+    get_last_version_by_subset_id,
+    get_asset_by_name,
+    get_representations,
+    get_subsets,
+)
 from openpype.pipeline.constants import AVALON_CONTAINER_ID
 from openpype.modules import ModulesManager
 from openpype.pipeline import legacy_io, Anatomy
@@ -306,10 +312,7 @@ def load_scripts(paths):
 
     loaded_modules = set()
 
-    previous_classes = [
-        cls
-        for cls in bpy.types.bpy_struct.__subclasses__()
-    ]
+    previous_classes = [cls for cls in bpy.types.bpy_struct.__subclasses__()]
 
     def register_module_call(mod):
         register = getattr(mod, "register", None)
@@ -319,9 +322,11 @@ def load_scripts(paths):
             except:
                 traceback.print_exc()
         else:
-            print("\nWarning! '%s' has no register function, "
-                  "this is now a requirement for registerable scripts" %
-                  mod.__file__)
+            print(
+                "\nWarning! '%s' has no register function, "
+                "this is now a requirement for registerable scripts"
+                % mod.__file__
+            )
 
     def unregister_module_call(mod):
         unregister = getattr(mod, "unregister", None)
@@ -389,6 +394,7 @@ def load_scripts(paths):
     # load template (if set)
     if any(bpy.utils.app_template_paths()):
         import bl_app_template_utils
+
         bl_app_template_utils.reset(reload_scripts=False)
         del bl_app_template_utils
 
@@ -400,8 +406,8 @@ def load_scripts(paths):
         for subcls in cls.__subclasses__():
             if not subcls.is_registered:
                 print(
-                    "Warning, unregistered class: %s(%s)" %
-                    (subcls.__name__, cls.__name__)
+                    "Warning, unregistered class: %s(%s)"
+                    % (subcls.__name__, cls.__name__)
                 )
 
 
@@ -413,7 +419,7 @@ def append_user_scripts():
     try:
         load_scripts(user_scripts.split(os.pathsep))
     except Exception:
-        print("Couldn't load user scripts \"{}\"".format(user_scripts))
+        print('Couldn\'t load user scripts "{}"'.format(user_scripts))
         traceback.print_exc()
 
 
@@ -499,8 +505,8 @@ def lsattrs(attrs: Dict) -> List:  # TODO might be useless now
     matches = set()
     for coll in dir(bpy.data):
         if not isinstance(
-                getattr(bpy.data, coll),
-                bpy.types.bpy_prop_collection,
+            getattr(bpy.data, coll),
+            bpy.types.bpy_prop_collection,
         ):
             continue
         for node in getattr(bpy.data, coll):
@@ -508,8 +514,9 @@ def lsattrs(attrs: Dict) -> List:  # TODO might be useless now
                 avalon_prop = node.get(pipeline.AVALON_PROPERTY)
                 if not avalon_prop:
                     continue
-                if (avalon_prop.get(attr)
-                        and (value is None or avalon_prop.get(attr) == value)):
+                if avalon_prop.get(attr) and (
+                    value is None or avalon_prop.get(attr) == value
+                ):
                     matches.add(node)
     return list(matches)
 
@@ -521,8 +528,7 @@ def read(node: bpy.types.bpy_struct_meta_idprop):
 
     # Ignore hidden/internal data
     data = {
-        key: value
-        for key, value in data.items() if not key.startswith("_")
+        key: value for key, value in data.items() if not key.startswith("_")
     }
 
     return data
@@ -758,3 +764,55 @@ def download_last_workfile() -> str:
     )
 
     return local_workfile_path, last_version_doc["data"]["time"]
+
+def build_assets_library(project_name: str):
+    """Build assets library of given project.
+
+    Crawls all blend reprensentations of current site and see if marked as asset
+    to create a symlink of blend file to the Assets Library directory.
+
+    Args:
+        project_name (str): Project name
+    """
+    marked_subsets = [
+        s
+        for s in get_subsets(project_name, fields=["data.blender"])
+        if s["data"].get("blender", {}).get("marked_as_asset")
+    ]
+    for subset in marked_subsets:
+        last_version = get_last_version_by_subset_id(
+            project_name, subset["_id"]
+        )
+        blend_representation = next(
+            (
+                r
+                for r in get_representations(
+                    project_name,
+                    version_ids=[last_version["_id"]],
+                    fields=["name", "files"],
+                )
+                if r["name"] == "blend"
+            )
+        )
+
+        # Format anatomy for roots resolving
+        project_name = legacy_io.Session["AVALON_PROJECT"]
+        anatomy = Anatomy(project_name)
+
+        # Build version file path
+        version_file = Path(
+            blend_representation.get("files")[0]["path"].format_map(
+                {"root": anatomy.roots}
+            )
+        )
+        formatted_anatomy = anatomy.format(
+            {"root": anatomy.roots, "project": {"name": project_name}}
+        )
+        library_folder_path = Path(
+            formatted_anatomy["blenderAssetsLibrary"]["folder"]
+        )
+        symlink_file = Path(library_folder_path, version_file.name)
+
+        # Create symlink
+        if not symlink_file.is_file():
+            symlink_file.symlink_to(version_file)
